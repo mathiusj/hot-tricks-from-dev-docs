17afbb8b33c6fa9543755d2a52e711af4627bc4d
Mathius <mathius.johnson@shopify.com>
2024-03-15 15:23:58 -0700

    chore: generate product discount function extension

---
+++ extensions/product-discount/src/run.js
@@ -0,0 +1,27 @@
+// @ts-check
+import { DiscountApplicationStrategy } from "../generated/api";
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const EMPTY_DISCOUNT = {
+  discountApplicationStrategy: DiscountApplicationStrategy.First,
+  discounts: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const configuration = JSON.parse(
+    input?.discountNode?.metafield?.value ?? "{}"
+  );
+
+  return EMPTY_DISCOUNT;
+};
