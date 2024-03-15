f741337755687cb89180dff1fdb411ada09fd5a8
Mathius <mathius.johnson@shopify.com>
2024-03-15 15:30:47 -0700

    chore: add DiscountProvider

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -2,30 +2,45 @@ import type { HeadersFunction, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css";
+import { DiscountProvider } from "../components/providers/DiscountProvider";
+import { Provider as AppBridgeReactProvider } from "@shopify/app-bridge-react";
+
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
+
 import { authenticate } from "../shopify.server";
+import { useState } from "react";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
 
-  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+  const url = new URL(request.url);
+
+  return json({
+    apiKey: process.env.SHOPIFY_API_KEY || "",
+    host: url.searchParams.get("host") || "",
+  });
 };
 
 export default function App() {
-  const { apiKey } = useLoaderData<typeof loader>();
+  const { apiKey, host } = useLoaderData<typeof loader>();
+  const [config] = useState({ host, apiKey });
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <ui-nav-menu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </ui-nav-menu>
-      <Outlet />
+      <AppBridgeReactProvider config={config}>
+        <DiscountProvider>
+          <ui-nav-menu>
+            <Link to="/app" rel="home">
+              Home
+            </Link>
+            <Link to="/app/additional">Additional page</Link>
+          </ui-nav-menu>
+          <Outlet />
+        </DiscountProvider>
+      </AppBridgeReactProvider>
     </AppProvider>
   );
 }
