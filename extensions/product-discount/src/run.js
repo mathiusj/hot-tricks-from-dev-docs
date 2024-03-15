8e27886c3d8639640c4fde636e2f4f50d10163ea
Mathius <mathius.johnson@shopify.com>
2024-03-15 15:27:33 -0700

    chore: update extension with config logic

--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -4,6 +4,8 @@ import { DiscountApplicationStrategy } from "../generated/api";
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Target} Target
+ * @typedef {import("../generated/api").ProductVariant} ProductVariant
  */
 
 /**
@@ -19,9 +21,53 @@ const EMPTY_DISCOUNT = {
  * @returns {FunctionRunResult}
  */
 export function run(input) {
+  // Define a type for your configuration, and parse it from the metafield
+  /**
+   * @type {{
+   *   quantity: number
+   *   percentage: number
+   * }}
+   */
   const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
+    input?.discountNode?.metafield?.value ?? "{}",
   );
+  if (!configuration.quantity || !configuration.percentage) {
+    return EMPTY_DISCOUNT;
+  }
 
-  return EMPTY_DISCOUNT;
-};
+  const targets = input.cart.lines
+    // Use the configured quantity instead of a hardcoded value
+    .filter(
+      (line) =>
+        line.quantity >= configuration.quantity &&
+        line.merchandise.__typename == "ProductVariant",
+    )
+    .map((line) => {
+      const variant = /** @type {ProductVariant} */ (line.merchandise);
+      return /** @type {Target} */ ({
+        productVariant: {
+          id: variant.id,
+        },
+      });
+    });
+
+  if (!targets.length) {
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        targets,
+        value: {
+          percentage: {
+            // Use the configured percentage instead of a hardcoded value
+            value: configuration.percentage.toString(),
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+  };
+}
