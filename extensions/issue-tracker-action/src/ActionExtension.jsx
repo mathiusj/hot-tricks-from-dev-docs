ac5afadf28f6220ceebd7f80995481d2d0aafaa3
Mathius <mathius.johnson@shopify.com>
2024-03-15 17:07:04 -0700

    feat: complete issue-tracker-action

--- extensions/issue-tracker-action/src/ActionExtension.jsx
+++ extensions/issue-tracker-action/src/ActionExtension.jsx
@@ -1,78 +1,92 @@
-import {useEffect, useState} from 'react';
+import { useCallback, useEffect, useState } from "react";
 import {
   reactExtension,
   useApi,
+  TextField,
   AdminAction,
-  BlockStack,
   Button,
-  Text,
-} from '@shopify/ui-extensions-react/admin';
+  TextArea,
+  Box,
+} from "@shopify/ui-extensions-react/admin";
+import { getIssues, updateIssues } from "./utils";
 
-// The target used here must match the target used in the extension's toml file (./shopify.extension.toml)
-const TARGET = 'admin.product-details.action.render';
+function generateId(allIssues) {
+  return !allIssues?.length ? 0 : allIssues[allIssues.length - 1].id + 1;
+}
+
+function validateForm({ title, description }) {
+  return {
+    isValid: Boolean(title) && Boolean(description),
+    errors: {
+      title: !title,
+      description: !description,
+    },
+  };
+}
+
+// The target used here must match the target used in the extension's .toml file at ./shopify.extension.toml
+const TARGET = "admin.product-details.action.render";
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
-  // The useApi hook provides access to several useful APIs like i18n, close, and data.
-  const {i18n, close, data} = useApi(TARGET);
-  console.log({data});
-  const [productTitle, setProductTitle] = useState('');
-  // Use direct API calls to fetch data from Shopify.
-  // See https://shopify.dev/docs/api/admin-graphql for more information about Shopify's GraphQL API
+  //connect with the extension's APIs
+  const { close, data } = useApi(TARGET);
+  const [issue, setIssue] = useState({ title: "", description: "" });
+  const [allIssues, setAllIssues] = useState([]);
+  const [formErrors, setFormErrors] = useState(null);
+  const { title, description } = issue;
+
   useEffect(() => {
-    (async function getProductInfo() {
-      const getProductQuery = {
-        query: `query Product($id: ID!) {
-          product(id: $id) {
-            title
-          }
-        }`,
-        variables: {id: data.selected[0].id},
-      };
+    getIssues(data.selected[0].id).then((issues) => setAllIssues(issues || []));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-      const res = await fetch("shopify:admin/api/graphql.json", {
-        method: "POST",
-        body: JSON.stringify(getProductQuery),
-      });
+  const onSubmit = useCallback(async () => {
+    const { isValid, errors } = validateForm(issue);
+    setFormErrors(errors);
 
-      if (!res.ok) {
-        console.error('Network error');
-      }
+    if (isValid) {
+      // Commit changes to the database
+      await updateIssues(data.selected[0].id, [
+        ...allIssues,
+        {
+          id: generateId(allIssues),
+          completed: false,
+          ...issue,
+        },
+      ]);
+      // Close the modal using the 'close' API
+      close();
+    }
+  }, [issue, data.selected, allIssues, close]);
 
-      const productData = await res.json();
-      setProductTitle(productData.data.product.title);
-    })();
-  }, [data.selected]);
   return (
-    // The AdminAction component provides an API for setting the title and actions of the Action extension wrapper.
     <AdminAction
-      primaryAction={
-        <Button
-          onPress={() => {
-            console.log('saving');
-            close();
-          }}
-        >
-          Done
-        </Button>
-      }
-      secondaryAction={
-        <Button
-          onPress={() => {
-            console.log('closing');
-            close();
-          }}
-        >
-          Close
-        </Button>
-      }
+      title="Create an issue"
+      primaryAction={<Button onPress={onSubmit}>Create</Button>}
+      secondaryAction={<Button onPress={close}>Cancel</Button>}
     >
-      <BlockStack>
-        {/* Set the translation values for each supported language in the locales directory */}
-        <Text fontWeight="bold">{i18n.translate('welcome', {TARGET})}</Text>
-        <Text>Current product: {productTitle}</Text>
-      </BlockStack>
+      <TextField
+        value={title}
+        error={formErrors?.title ? "Please enter a title" : undefined}
+        onChange={(val) => setIssue((prev) => ({ ...prev, title: val }))}
+        label="Title"
+        maxLength={50}
+      />
+      <Box paddingBlockStart="large">
+        <TextArea
+          value={description}
+          error={
+            formErrors?.description ? "Please enter a description" : undefined
+          }
+          onChange={(val) =>
+            setIssue((prev) => ({ ...prev, description: val }))
+          }
+          label="Description"
+          maxLength={300}
+        />
+      </Box>
     </AdminAction>
   );
 }
