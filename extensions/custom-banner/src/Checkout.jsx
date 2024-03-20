3b03e725281fd8f08fb3fd44d63ae98e1689c4cc
Mathius <mathius.johnson@shopify.com>
2024-03-20 16:15:26 -0700

    feat: update custom-banner checkout extension markup

--- extensions/custom-banner/src/Checkout.jsx
+++ extensions/custom-banner/src/Checkout.jsx
@@ -1,22 +1,40 @@
+import React from "react";
 import {
-  Banner,
-  useApi,
-  useTranslate,
   reactExtension,
-} from '@shopify/ui-extensions-react/checkout';
+  Banner,
+  useSettings,
+} from "@shopify/ui-extensions-react/checkout";
 
-export default reactExtension(
-  'purchase.checkout.block.render',
-  () => <Extension />,
+// Set the entry points for the extension
+const checkoutBlock = reactExtension("purchase.checkout.block.render", () => (
+  <App />
+));
+export { checkoutBlock };
+
+const deliveryAddress = reactExtension(
+  "purchase.checkout.delivery-address.render-before",
+  () => <App />,
 );
+export { deliveryAddress };
 
-function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
+function App() {
+  // Use the merchant-defined settings to retrieve the extension's content
+  // Use the merchant-defined settings to retrieve the extension's content
+  const {
+    title: merchantTitle,
+    description,
+    collapsible,
+    status: merchantStatus,
+  } = useSettings();
 
+  // Set a default status for the banner if a merchant didn't configure the banner in the checkout editor
+  const status = merchantStatus ?? "info";
+  const title = merchantTitle ?? "Custom Banner";
+
+  // Render the banner
   return (
-    <Banner title="custom-banner">
-      {translate('welcome', {target: extension.target})}
+    <Banner title={title} status={status} collapsible={collapsible}>
+      {description}
     </Banner>
   );
 }
