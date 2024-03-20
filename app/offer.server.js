adc67495bd1929b76ffd32c64200cf9adf2b2ff1
Mathius <mathius.johnson@shopify.com>
2024-03-20 15:52:47 -0700

    feat: add offer.server that contains my snowoboard data

---
+++ app/offer.server.js
@@ -0,0 +1,42 @@
+const OFFERS = [
+  {
+    id: 1,
+    title: "One time offer",
+    productTitle: "The S-Series Snowboard",
+    productImageURL:
+      "https://cdn.shopify.com/s/files/1/0781/2870/9915/files/Main.jpg?v=1687807652", // Replace this with the product image's URL.
+    productDescription: ["This PREMIUM snowboard is so SUPER DUPER awesome!"],
+    originalPrice: "699.95",
+    discountedPrice: "699.95",
+    changes: [
+      {
+        type: "add_variant",
+        variantID: 8355095937307, // Replace with the variant ID.
+        quantity: 1,
+        discount: {
+          value: 15,
+          valueType: "percentage",
+          title: "15% off",
+        },
+      },
+    ],
+  },
+];
+
+/*
+ * For testing purposes, product information is hardcoded.
+ * In a production application, replace this function with logic to determine
+ * what product to offer to the customer.
+ */
+export function getOffers() {
+  return OFFERS;
+}
+
+/*
+ * Retrieve discount information for the specific order on the backend instead of relying
+ * on the discount information that is sent from the frontend.
+ * This is to ensure that the discount information is not tampered with.
+ */
+export function getSelectedOffer(offerId) {
+  return OFFERS.find((offer) => offer.id === offerId);
+}
