aa0a4b43d123f5b2d1d4618623921561ed17db6e
Mathius <mathius.johnson@shopify.com>
2024-03-21 09:03:01 -0700

    feat: generate payment-customization function

---
+++ extensions/payment-customization/src/run.js
@@ -0,0 +1,25 @@
+// @ts-check
+
+/**
+ * @typedef {import("../generated/api").RunInput} RunInput
+ * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ */
+
+/**
+ * @type {FunctionRunResult}
+ */
+const NO_CHANGES = {
+  operations: [],
+};
+
+/**
+ * @param {RunInput} input
+ * @returns {FunctionRunResult}
+ */
+export function run(input) {
+  const configuration = JSON.parse(
+    input?.paymentCustomization?.metafield?.value ?? "{}"
+  );
+
+  return NO_CHANGES;
+};
