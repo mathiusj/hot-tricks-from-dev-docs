65ed5c69243a2c562bbd947db03023d9b14aee7b
Mathius <mathius.johnson@shopify.com>
2024-03-21 08:25:35 -0700

    feat: update UI in client-validation extension

--- extensions/client-validation/src/Checkout.jsx
+++ extensions/client-validation/src/Checkout.jsx
@@ -1,22 +1,84 @@
 import {
-  Banner,
-  useApi,
-  useTranslate,
   reactExtension,
-} from '@shopify/ui-extensions-react/checkout';
+  TextField,
+  useExtensionCapability,
+  useBuyerJourneyIntercept,
+} from "@shopify/ui-extensions-react/checkout";
+import React, { useState } from "react";
+// Set the entry point for the extension
+export default reactExtension("purchase.checkout.contact.render-after", () => (
+  <App />
+));
 
-export default reactExtension(
-  'purchase.checkout.block.render',
-  () => <Extension />,
-);
+function App() {
+  // Set the target age that a buyer must be to complete an order
+  const ageTarget = 18;
 
-function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
+  // Set up the app state
+  const [age, setAge] = useState("");
+  const [validationError, setValidationError] = useState("");
+  // Merchants can toggle the `block_progress` capability behavior within the checkout editor
+  const canBlockProgress = useExtensionCapability("block_progress");
+  const label = canBlockProgress ? "Your age" : "Your age (optional)";
+  // Use the `buyerJourney` intercept to conditionally block checkout progress
+  useBuyerJourneyIntercept(({ canBlockProgress }) => {
+    // Validate that the age of the buyer is known, and that they're old enough to complete the purchase
+    if (canBlockProgress && !isAgeSet()) {
+      return {
+        behavior: "block",
+        reason: "Age is required",
+        perform: (result) => {
+          // If progress can be blocked, then set a validation error on the custom field
+          if (result.behavior === "block") {
+            setValidationError("Enter your age");
+          }
+        },
+      };
+    }
 
+    if (canBlockProgress && !isAgeValid()) {
+      return {
+        behavior: "block",
+        reason: `Age is less than ${ageTarget}.`,
+        errors: [
+          {
+            // Show a validation error on the page
+            message:
+              "You're not legally old enough to buy some of the items in your cart.",
+          },
+        ],
+      };
+    }
+
+    return {
+      behavior: "allow",
+      perform: () => {
+        // Ensure any errors are hidden
+        clearValidationErrors();
+      },
+    };
+  });
+  function isAgeSet() {
+    return age !== "";
+  }
+
+  function isAgeValid() {
+    return Number(age) >= ageTarget;
+  }
+
+  function clearValidationErrors() {
+    setValidationError("");
+  }
+  // Render the extension
   return (
-    <Banner title="client-validation">
-      {translate('welcome', {target: extension.target})}
-    </Banner>
+    <TextField
+      label={label}
+      type="number"
+      value={age}
+      onChange={setAge}
+      onInput={clearValidationErrors}
+      required={canBlockProgress}
+      error={validationError}
+    />
   );
 }
