9a6e5a1f9bd78199a0a98a30918857e68a85bb48
Mathius <mathius.johnson@shopify.com>
2024-03-21 08:53:39 -0700

    feat: use extension in thank you

---
+++ extensions/custom-footer/src/ThankYou.jsx
@@ -0,0 +1,7 @@
+import { reactExtension } from "@shopify/ui-extensions-react/checkout";
+
+import Extension from "./Extension.jsx";
+
+export default reactExtension("purchase.thank-you.footer.render-after", () => (
+  <Extension />
+));
