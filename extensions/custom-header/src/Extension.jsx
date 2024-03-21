56893159cd252a785f7056da233bc7e4f2dd7296
Mathius <mathius.johnson@shopify.com>
2024-03-21 08:38:16 -0700

    chore: add Extension component to custom-header extension

---
+++ extensions/custom-header/src/Extension.jsx
@@ -0,0 +1,9 @@
+import { Image } from "@shopify/ui-extensions-react/checkout";
+
+export default function Extension() {
+  return (
+    // Replace the source with your own image url. Learn more:
+    // https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
+    <Image source="https://cdn.shopify.com/path/to/image/file_name.png" />
+  );
+}
