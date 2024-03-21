d7d7b7361faef966c36c59417111944f375820a8
Mathius <mathius.johnson@shopify.com>
2024-03-21 08:42:28 -0700

    chore: add cool lambo to extension image source

--- extensions/custom-header/src/Extension.jsx
+++ extensions/custom-header/src/Extension.jsx
@@ -4,6 +4,6 @@ export default function Extension() {
   return (
     // Replace the source with your own image url. Learn more:
     // https://help.shopify.com/en/manual/shopify-admin/productivity-tools/file-uploads
-    <Image source="https://cdn.shopify.com/path/to/image/file_name.png" />
+    <Image source="https://cdn.shopify.com/s/files/1/0781/2870/9915/files/cool-lamborghinis-pictures.jpg?v=1711035683" />
   );
 }
