beef5e51d55853034bd4e915b077aefbd697cc45
Mathius <mathius.johnson@shopify.com>
2024-03-21 08:38:25 -0700

    chore: add ThankYou component to custom-header extension

---
+++ extensions/custom-header/src/ThankYou.jsx
@@ -0,0 +1,7 @@
+import { reactExtension } from "@shopify/ui-extensions-react/checkout";
+
+import Extension from "./Extension.jsx";
+
+export default reactExtension("purchase.thank-you.header.render-after", () => (
+  <Extension />
+));
