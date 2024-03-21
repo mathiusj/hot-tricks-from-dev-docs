4606c0b1788f2be37d86bfe1db8e1b9e4d86fde7
Mathius <mathius.johnson@shopify.com>
2024-03-21 10:44:38 -0700

    chore: use metafields in run.js

--- extensions/payment-customization/src/run.js
+++ extensions/payment-customization/src/run.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-// Use JSDoc annotations for type safety
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
@@ -13,33 +12,43 @@ const NO_CHANGES = {
   operations: [],
 };
 
-// The configured entrypoint for the 'purchase.payment-customization.run' extension target
 /**
  * @param {RunInput} input
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  // Get the cart total from the function input, and return early if it's below 100
+  // Define a type for your configuration, and parse it from the metafield
+  /**
+   * @type {{
+   *   paymentMethodName: string
+   *   cartTotal: number
+   * }}
+   */
+  const configuration = JSON.parse(
+    input?.paymentCustomization?.metafield?.value ?? "{}",
+  );
+  if (!configuration.paymentMethodName || !configuration.cartTotal) {
+    return NO_CHANGES;
+  }
+
   const cartTotal = parseFloat(input.cart.cost.totalAmount.amount ?? "0.0");
-  if (cartTotal < 100) {
-    // You can use STDERR for debug logs in your function
+  // Use the configured cart total instead of a hardcoded value
+  if (cartTotal < configuration.cartTotal) {
     console.error(
       "Cart total is not high enough, no need to hide the payment method.",
     );
     return NO_CHANGES;
   }
 
-  // Find the payment method to hide
+  // Use the configured payment method name instead of a hardcoded value
   const hidePaymentMethod = input.paymentMethods.find((method) =>
-    method.name.includes("Cash on Delivery"),
+    method.name.includes(configuration.paymentMethodName),
   );
 
   if (!hidePaymentMethod) {
     return NO_CHANGES;
   }
 
-  // The @shopify/shopify_function package applies JSON.stringify() to your function result
-  // and writes it to STDOUT
   return {
     operations: [
       {
