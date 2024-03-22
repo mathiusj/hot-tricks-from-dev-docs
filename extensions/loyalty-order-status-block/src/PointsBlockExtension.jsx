012ea967f52fb298deb96125b43587f4f3810d34
Mathius <mathius.johnson@shopify.com>
2024-03-22 16:07:21 -0700

    feat: generate loyalty extension fulfillment

---
+++ extensions/loyalty-order-status-block/src/PointsBlockExtension.jsx
@@ -0,0 +1,25 @@
+import {
+  BlockStack,
+  reactExtension,
+  TextBlock,
+  Link,
+  Banner,
+} from "@shopify/ui-extensions-react/customer-account";
+
+export default reactExtension(
+  "customer-account.order-status.block.render",
+  () => <PromotionBanner />,
+);
+
+function PromotionBanner() {
+  return (
+    <Banner>
+      <BlockStack inlineAlignment="center">
+        <TextBlock>
+          {`🎉 You've earned 1,000 points from this order. You've been upgraded to Platinum tier. `}
+          <Link>View rewards</Link>
+        </TextBlock>
+      </BlockStack>
+    </Banner>
+  );
+}
