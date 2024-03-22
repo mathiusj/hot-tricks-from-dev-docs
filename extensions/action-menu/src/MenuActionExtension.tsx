0e72314a0fa884f4a523d720dffc6ceacb9599ac
Mathius <mathius.johnson@shopify.com>
2024-03-22 16:24:02 -0700

    chore: update UI in MenuActionExtension

--- extensions/action-menu/src/MenuActionExtension.tsx
+++ extensions/action-menu/src/MenuActionExtension.tsx
@@ -1,11 +1,49 @@
-import { Button, reactExtension, useApi } from '@shopify/ui-extensions-react/customer-account';
+import {
+  Button,
+  reactExtension,
+} from "@shopify/ui-extensions-react/customer-account";
 
 export default reactExtension(
   "customer-account.order.action.menu-item.render",
-  () => <MenuActionExtension />
+  async (api) => {
+    const { orderId } = api;
+    let hasFulfillments = false;
+    try {
+      const orderQuery = {
+        query: `query {
+          order(id: "${orderId}") {
+            fulfillments(first: 1) {
+              nodes {
+                latestShipmentStatus
+              }
+            }
+          }
+        }`,
+      };
+      const result = await fetch(
+        "shopify://customer-account/api/latest/graphql.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(orderQuery),
+        },
+      );
+      const { data } = await result.json();
+      hasFulfillments = data.order.fulfillments.nodes.length !== 0;
+    } catch (error) {
+      console.log(error);
+      hasFulfillments = false;
+    }
+    return <MenuActionExtension showAction={hasFulfillments} />;
+  },
 );
 
-function MenuActionExtension() {
-  const {i18n} = useApi()
-  return <Button to='extension:/'>{i18n.translate("menuItemButton")}</Button>;
+function MenuActionExtension({ showAction }: { showAction: boolean }) {
+  if (!showAction) {
+    return null;
+  }
+
+  return <Button>Report a problem</Button>;
 }
