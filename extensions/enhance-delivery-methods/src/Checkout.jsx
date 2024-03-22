4bc448cb686ba05e25e70277afe06d9ae29e04e1
Mathius <mathius.johnson@shopify.com>
2024-03-22 12:36:09 -0700

    chore: complete sample code in delivery-methods extension

--- extensions/enhance-delivery-methods/src/Checkout.jsx
+++ extensions/enhance-delivery-methods/src/Checkout.jsx
@@ -2,6 +2,9 @@ import { useState, useCallback, useMemo } from "react";
 import {
   Heading,
   DatePicker,
+  useApplyMetafieldsChange,
+  useDeliveryGroups,
+  useApi,
   reactExtension,
 } from "@shopify/ui-extensions-react/checkout";
 
@@ -12,6 +15,18 @@ reactExtension("purchase.checkout.shipping-option-list.render-after", () => (
 export default function Extension() {
   const [selectedDate, setSelectedDate] = useState("");
   const [yesterday, setYesterday] = useState("");
+
+  const { extension } = useApi();
+  const { target } = extension;
+
+  let deliveryGroups = useDeliveryGroups();
+
+  // Set a function to handle updating a metafield
+  const applyMetafieldsChange = useApplyMetafieldsChange();
+
+  // Define the metafield namespace and key
+  const metafieldNamespace = "yourAppNamespace";
+  const metafieldKey = "deliverySchedule";
 
   // Sets the selected date to today, unless today is Sunday, then it sets it to tomorrow
   useMemo(() => {
@@ -30,11 +45,41 @@ export default function Extension() {
   }, []);
 
   // Set a function to handle the Date Picker component's onChange event
-  const handleChangeDate = useCallback((selectedDate) => {
-    setSelectedDate(selectedDate);
-  }, []);
+  const handleChangeDate = useCallback(
+    (selectedDate) => {
+      setSelectedDate(selectedDate);
+      // Apply the change to the metafield
+      applyMetafieldsChange({
+        type: "updateMetafield",
+        namespace: metafieldNamespace,
+        key: metafieldKey,
+        valueType: "string",
+        value: selectedDate,
+      });
+    },
+    [applyMetafieldsChange],
+  );
 
-  return (
+  // Boolean to check if Express is selected
+  const isExpressSelected = () => {
+    if (
+      target !== "purchase.checkout.shipping-option-list.render-after" ||
+      !deliveryGroups
+    ) {
+      return false;
+    }
+
+    const expressHandle = deliveryGroups[0].deliveryOptions.find(
+      (method) => method.title === "Express",
+    )?.handle;
+
+    return expressHandle === deliveryGroups[0].selectedDeliveryOption?.handle
+      ? true
+      : false;
+  };
+
+  // Render the extension components if Express is selected
+  return isExpressSelected() ? (
     <>
       <Heading>Select a date for delivery</Heading>
       <DatePicker
@@ -43,7 +88,7 @@ export default function Extension() {
         disabled={["Sunday", { end: yesterday }]}
       />
     </>
-  );
+  ) : null;
 }
 
 const formatDate = (date) => {
