045367de647373ba610dc2ca62a4e2e835c039e2
Mathius <mathius.johnson@shopify.com>
2024-03-22 12:33:38 -0700

    feat: update UI for delivery-method extension

--- extensions/enhance-delivery-methods/src/Checkout.jsx
+++ extensions/enhance-delivery-methods/src/Checkout.jsx
@@ -1,22 +1,54 @@
+import { useState, useCallback, useMemo } from "react";
 import {
-  Banner,
-  useApi,
-  useTranslate,
+  Heading,
+  DatePicker,
   reactExtension,
-} from '@shopify/ui-extensions-react/checkout';
+} from "@shopify/ui-extensions-react/checkout";
 
-export default reactExtension(
-  'purchase.checkout.block.render',
-  () => <Extension />,
-);
+reactExtension("purchase.checkout.shipping-option-list.render-after", () => (
+  <Extension />
+));
 
-function Extension() {
-  const translate = useTranslate();
-  const { extension } = useApi();
+export default function Extension() {
+  const [selectedDate, setSelectedDate] = useState("");
+  const [yesterday, setYesterday] = useState("");
+
+  // Sets the selected date to today, unless today is Sunday, then it sets it to tomorrow
+  useMemo(() => {
+    let today = new Date();
+
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
+    const deliveryDate = today.getDay() === 0 ? tomorrow : today;
+
+    setSelectedDate(formatDate(deliveryDate));
+    setYesterday(formatDate(yesterday));
+  }, []);
+
+  // Set a function to handle the Date Picker component's onChange event
+  const handleChangeDate = useCallback((selectedDate) => {
+    setSelectedDate(selectedDate);
+  }, []);
 
   return (
-    <Banner title="enhance-delivery-methods">
-      {translate('welcome', {target: extension.target})}
-    </Banner>
+    <>
+      <Heading>Select a date for delivery</Heading>
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleChangeDate}
+        disabled={["Sunday", { end: yesterday }]}
+      />
+    </>
   );
 }
+
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+};
