2c4989d36e241b318e47b8c8fcd56f7721ce6093
Mathius <mathius.johnson@shopify.com>
2024-03-21 17:21:56 -0700

    feat: add complex validation logic to cart validation 2

--- extensions/cart-checkout-validation-2/src/run.js
+++ extensions/cart-checkout-validation-2/src/run.js
@@ -9,15 +9,25 @@
  * @param {RunInput} input
  * @returns {FunctionRunResult}
  */
-export function run({ cart }) {
-  const errors = cart.lines
-    .filter(({ quantity }) => {
-      return quantity > 1;
-    })
-    .map(() => ({
-      localizedMessage: "Not possible to order more than 1",
-      target: "cart",
-    }));
+export function run({ cart, validation }) {
+  /** @type {Array<{productVariantId: string; quantityLimit: number}>} */
+  const configuration = JSON.parse(validation?.metafield?.value ?? "{}");
+
+  const errors = cart.lines.reduce((acc, { quantity, merchandise }) => {
+    if ("id" in merchandise) {
+      const limit = configuration[merchandise.id] || Infinity;
+      const title = merchandise.product.title;
+
+      if (quantity > limit) {
+        acc.push({
+          localizedMessage: `Not possible to order more than ${quantity} of ${title}`,
+          target: "cart",
+        });
+      }
+    }
+
+    return acc;
+  }, []);
 
   return {
     errors,
