578725c3d116fb5fa8f848804264d297149d7718
Mathius <mathius.johnson@shopify.com>
2024-03-22 16:03:55 -0700

    feat: generate customer UI extension for loyalty

--- extensions/local-pickup-generator/src/index.js
+++ extensions/local-pickup-generator/src/index.js
@@ -1,36 +1,50 @@
 // @ts-check
 
+// Use JSDoc annotations for type safety.
 /**
  * @typedef {import("../generated/api").InputQuery} InputQuery
  * @typedef {import("../generated/api").FunctionResult} FunctionResult
+ * @typedef {import("../generated/api").Operation} Operation
  */
 
-/**
- * @type {FunctionResult}
- */
-const DELIVERY_OPTION = {
-  operations: [
-    {
-      add: {
-        title: "Main St.",
-        cost: 1.99,
-        pickupLocation: {
-          locationHandle: "2578303",
-          pickupInstruction: "Usually ready in 24 hours."
-        }
-      }
-    }
-  ],
-};
-
+// The @shopify/shopify_function package will use the default export as your function entrypoint.
 export default /**
  * @param {InputQuery} input
  * @returns {FunctionResult}
  */
 (input) => {
-  const configuration = JSON.parse(
-    input?.deliveryOptionGenerator?.metafield?.value ?? "{}"
-  );
+  const hasBulkyVariant = input.cart.lines.some((line) => {
+    switch (line.merchandise.__typename) {
+      case "ProductVariant":
+        return line.merchandise.product.hasAnyTag;
+      case "CustomProduct":
+        return false;
+      default:
+        return false;
+    }
+  });
 
-  return DELIVERY_OPTION;
+  let cost;
+  let pickupInstruction;
+  if (hasBulkyVariant) {
+    cost = 2.99;
+    pickupInstruction = "Ready for pickup next business day.";
+  } else {
+    cost = 0.0;
+    pickupInstruction = "Ready for pickup now!";
+  }
+
+  const operations = input.locations.map((location) => ({
+    add: {
+      title: location.name,
+      cost: cost,
+      pickupLocation: {
+        locationHandle: location.handle,
+        pickupInstruction: pickupInstruction,
+      },
+    },
+  }));
+
+  // Return the operations
+  return { operations: operations };
 };
